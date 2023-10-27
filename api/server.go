e6c7c31d6b4206afd485b8f039b722347f00ca4d
Joaquin Damian Seita <joaquindamianseita@gmail.com>
2023-10-26 23:13:56 -0300

    add release mode for test

--- api/server.go
+++ api/server.go
@@ -4,11 +4,16 @@ import (
 	"GoGin-API-CuentasClaras/api/middleware"
 	"GoGin-API-CuentasClaras/api/routes"
 	"GoGin-API-CuentasClaras/config"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Init(init *config.Initialization) *gin.Engine {
+	apiMode := os.Getenv("ENVIROMENT")
+	if apiMode == "production" || apiMode == "test" {
+		gin.SetMode(gin.ReleaseMode)
+	}
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
