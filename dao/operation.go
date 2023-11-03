c40e78b1c8d93b06c281cd653e7f9c1617849b7e
JoaquinDamianSeita <74945252+JoaquinDamianSeita@users.noreply.github.com>
2023-11-02 21:27:05 -0300

    fix operation dao (#7)

--- dao/operation.go
+++ dao/operation.go
@@ -5,12 +5,13 @@ import (
 )
 
 type Operation struct {
-	ID          int       `gorm:"column:id; primary_key; not null" json:"id"`
-	UserID      uint      `json:"-"`
-	Category    Category  `gorm:"foreignKey:ID" json:"category"`
-	Type        string    `json:"type"`
-	Amount      float64   `json:"amount"`
-	Date        time.Time `json:"date"`
-	Description string    `json:"description"`
-	BaseModel
+    ID          int       `gorm:"column:id; primary_key; not null" json:"id"`
+    UserID      uint      `json:"-"`
+    CategoryID  int       `json:"category_id"`
+    Category    Category  `gorm:"foreignKey:CategoryID" json:"category"`
+    Type        string    `json:"type"`
+    Amount      float64   `json:"amount"`
+    Date        time.Time `json:"date"`
+    Description string    `json:"description"`
+    BaseModel
 }
